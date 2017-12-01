8f380552c52dba9b6bc06828f052d56d8fba482d
Jms Dnns <jdennis@gmail.com>
2017-12-01 12:47:54 -0500

    Added tagbar, vim-markdown, vim-scala, and typescript-vim

--- setup.sh
+++ setup.sh
@@ -13,10 +13,22 @@ wget https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathog
 
 ## Install Plugins
 cd ~/.vim/bundle
+
+### Nordic theme
+git clone https://github.com/arcticicestudio/nord-vim.git
+
+### UI Enhancements
+git clone https://github.com/vim-airline/vim-airline.git
 git clone https://github.com/ctrlpvim/ctrlp.vim
 git clone https://github.com/scrooloose/nerdtree.git
-git clone https://github.com/vim-airline/vim-airline.git
-git clone https://github.com/arcticicestudio/nord-vim.git
+git clone https://github.com/majutsushi/tagbar
+
+### :Bd command for closing buffer without closing tab
 git clone https://github.com/moll/vim-bbye
+
+### Languages
+git clone https://github.com/plasticboy/vim-markdown
+git clone https://github.com/derekwyatt/vim-scala
+git clone https://github.com/leafgarland/typescript-vim.git
 
 cd $ORIGDIR
